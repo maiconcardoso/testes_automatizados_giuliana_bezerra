5ce70a3ea3fd9c8c09283c149c9288b651d9450d
Maicon Cardoso <maiconscardoso@hotmail.com>
2024-10-27 17:48:09 -0300

    Testando o cenário de erros

--- project/src/test/java/br/com/maicon/project/common/PlanetConstants.java
+++ project/src/test/java/br/com/maicon/project/common/PlanetConstants.java
@@ -5,4 +5,5 @@ import br.com.maicon.project.domain.Planet;
 public class PlanetConstants {
 
     public static final Planet PLANET = new Planet("name", "climate", "Terrain"); 
+    public static final Planet INVALID_PLANET = new Planet("", "", ""); 
 }
--- project/src/test/java/br/com/maicon/project/services/PlanetServiceTest.java
+++ project/src/test/java/br/com/maicon/project/services/PlanetServiceTest.java
@@ -1,6 +1,7 @@
 package br.com.maicon.project.services;
 
 import static br.com.maicon.project.common.PlanetConstants.PLANET;
+import static br.com.maicon.project.common.PlanetConstants.INVALID_PLANET;
 import static org.mockito.Mockito.when;
 
 import org.assertj.core.api.Assertions;
@@ -28,6 +29,12 @@ public class PlanetServiceTest {
         when(repository.save(PLANET)).thenReturn(PLANET);
         Planet sut = service.create(PLANET);
         Assertions.assertThat(sut).isEqualTo(PLANET);
+    }
+
+    @Test
+    public void createPlanet_withDataInvalid_throwsException() {
+        when(repository.save(INVALID_PLANET)).thenThrow(RuntimeException.class);
+        Assertions.assertThatThrownBy(() -> service.create(INVALID_PLANET)).isInstanceOf(RuntimeException.class);
     }
 
 }
